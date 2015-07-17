9209cc0c7514acb37963697f16315cb2f4635411
fight <awfwxf@qq.com>
2015-07-17 13:32:24 +0800

    service user

--- routes/api-router.js
+++ routes/api-router.js
@@ -21,10 +21,66 @@ var exports = module.exports;
 function router(req, res) {
 
     //Use req.params, req.body, or req.query instead
+    var apiType=req.query.type;
+    var openId=req.query.openid;
+    var orderNo=req.query.orderno;
 
-    console.log(req.param('type'));
-    console.log(req.param('openid'));
-    console.log(req.param('taskid'));
+    /*
+        调用结果,result结果返回码：
+        0：成功
+        10：无效的type类型
+        11：无效的openid
+        12：无效的orderno
+        13：openid或orderno为空
+
+        出现下述错误，回复体中会包含msg存储具体的错误描述
+        20: 调用微信发送消息接口时异常
+        21: 微信服务器返回的错误
+     */
+    var resutlObj=new Object();
+    resutlObj.msg='';
+
+    if(apiType){
+        if(apiType === 'sendmsg'){
+            if(openId && orderNo){
+                //发送选择并上传小视频的消息给指定用户
+                sendUploadVideoMsgToUser(openId,function(error, response, body) {
+
+                    if(error){
+                        resutlObj.result='20';
+                        resutlObj.msg=error;
+                        res.end(JSON.stringify(resutlObj));
+                    }else{
+                        if(body.errcode===0){
+                            //成功
+                            resutlObj.result='0';
+                            res.end(JSON.stringify(resutlObj));
+                        }else{
+                            //失败
+                            resutlObj.result='21';
+                            resutlObj.msg=body.errcode+' : '+body.errmsg;
+                            res.end(JSON.stringify(resutlObj));
+                        }
+                    }
+                });
+            }else{
+                resutlObj.result='13';
+                //回复调用结果
+                res.end(JSON.stringify(resutlObj));
+            }
+        }else{
+            //没有匹配的type
+            resutlObj.result='10';
+            //回复调用结果
+            res.end(JSON.stringify(resutlObj));
+        }
+    }else{
+        resutlObj.result='10';
+        //回复调用结果
+        res.end(JSON.stringify(resutlObj));
+    }
+
+
 
     //向指定用户发送消息
     /*var msgObj={
@@ -40,7 +96,7 @@ function router(req, res) {
     });*/
 
     //七牛fetch资源
-    var url='http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=bgfZ1dX-Qh4EBCXFRbhYU52DRXzOD8cetjekqOMqLnNuRLZF3rmZ4scmZg40b626MtHiwFNOW4KxccvZBy_I14w4qLt894siMGXRbm1De-g&media_id=yZomEIPXk9R3InL_3q4L9d604L_42Hsv61_XWU_lXeBOGB8MNOdk1tFql00vJ-er';
+    /*var url='http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=bgfZ1dX-Qh4EBCXFRbhYU52DRXzOD8cetjekqOMqLnNuRLZF3rmZ4scmZg40b626MtHiwFNOW4KxccvZBy_I14w4qLt894siMGXRbm1De-g&media_id=yZomEIPXk9R3InL_3q4L9d604L_42Hsv61_XWU_lXeBOGB8MNOdk1tFql00vJ-er';
     qiniuUtil.fetch(url,'wechatShortVideo.mp4',function(error, res, body) {
         if(error){
             console.log(error);
@@ -48,10 +104,21 @@ function router(req, res) {
             console.log(res.statusCode);
             console.log(body);
         }
-    });
+    });*/
 
-
-    res.end('done....');
 }
 
 exports.router=router;
+
+
+function sendUploadVideoMsgToUser(openid,callback){
+    var msgObj={
+        touser: openid,
+        msgtype:"text",
+        text:
+        {
+            content: "已收到前面提交的任务反馈，请点选右下角的+，然后选择小视频"
+        }
+    };
+    utils.sendMsgToUser(msgObj,callback);
+}
